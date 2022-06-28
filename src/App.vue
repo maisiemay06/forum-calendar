c2ef0e14e9990af81204c89083b5633d1188230a
maisiemay06 <73712593+maisiemay06@users.noreply.github.com>
2022-06-28 11:14:52 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <nav-bar></nav-bar>
+</template>
+
+<script>
+import NavBar from "./components/NavBar.vue";
+
+export default {
+  name: "App",
+  components: {
+    NavBar,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: "Sofia Pro", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+}
+</style>
