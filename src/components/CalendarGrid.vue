735718ebc62ce8542a99d9cf2d7500075059a5e7
maisiemay06 <73712593+maisiemay06@users.noreply.github.com>
2022-06-29 12:35:12 +0100

    Add event data, format and style events

--- src/components/CalendarGrid.vue
+++ src/components/CalendarGrid.vue
@@ -9,6 +9,7 @@ import FullCalendar from "@fullcalendar/vue3";
 import interactionPlugin from "@fullcalendar/interaction";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import moment from "moment";
+import EventData from "./EventData";
 
 export default {
   name: "CalendarGrid",
@@ -18,6 +19,7 @@ export default {
   data() {
     return {
       options: {
+        events: EventData,
         themeSystem: "standard",
         plugins: [timeGridPlugin, interactionPlugin],
         scrollTime: "09:00:00",
@@ -31,7 +33,7 @@ export default {
         allDaySlot: false,
         firstDay: 0,
         expandRows: true,
-        height: 800,
+        height: "70vh",
         dayHeaderFormat: { weekday: "short", day: "numeric" },
         slotLabelFormat: {
           hour: "numeric",
@@ -39,10 +41,18 @@ export default {
           hour12: true,
         },
         nowIndicator: true,
+        eventBackgroundColor: "#fff",
+        eventTextColor: "#000",
+        eventTimeFormat: {
+          hour: "numeric",
+          minute: "2-digit",
+          meridiem: "lowercase",
+          omitZeroMinute: true,
+        },
+        displayEventEnd: false,
       },
     };
   },
-  methods: {},
 };
 </script>
 
@@ -108,5 +118,38 @@ table tr td:last-child {
 
 .fc-col-header-cell {
   background-color: transparent !important;
+}
+
+.fc-timegrid-event {
+  border-radius: 4px 0 4px 0 !important;
+  font-size: 12px !important;
+}
+.fc-event-time {
+  font-weight: 600 !important;
+  font-size: 12px !important;
+  margin-top: 3px;
+}
+.fc-v-event .fc-event-main-frame {
+  flex-direction: column-reverse !important;
+  justify-content: start;
+  padding-left: 15px !important;
+  position: relative;
+  top: -100%;
+}
+.fc-v-event .fc-event-title-container {
+  flex-grow: 0 !important;
+  flex-shrink: 0 !important;
+}
+.fc-event-main::before {
+  content: "";
+  height: 102%;
+  background-color: #394ced;
+  border-color: #394ced;
+  border-radius: 4px 0 4px 0;
+  width: 10px !important;
+  display: inline-block;
+  position: relative;
+  top: -1px;
+  left: -1px;
 }
 </style>
---
+++ src/components/EventData.js
@@ -0,0 +1,83 @@
+export default [
+  {
+    id: 1,
+    title: "Is AI destroying the design indistry?",
+    start: "2022-06-26T06:00:00",
+    end: "2022-06-26T07:00:00",
+    borderColor: "#9231CE",
+    extendedProps: {
+      category: "bookedMeeow",
+    },
+  },
+  {
+    id: 2,
+    title: "On-demand networking",
+    start: "2022-06-26T11:00:00",
+    end: "2022-06-26T11:45:00",
+    borderColor: "#394CED",
+    extendedProps: {
+      category: "onDemandMeeow",
+    },
+  },
+  {
+    id: 3,
+    title: "Created Event",
+    start: "2022-06-26T13:30:00",
+    end: "2022-06-26T14:30:00",
+    borderColor: "#00DCDC",
+    extendedProps: {
+      category: "createdMeeow",
+    },
+  },
+  {
+    id: 4,
+    title: "Created Event",
+    start: "2022-06-26T17:00:00",
+    end: "2022-06-26T19:00:00",
+    borderColor: "#00DCDC",
+    extendedProps: {
+      category: "createdMeeow",
+    },
+  },
+
+  {
+    id: 5,
+    title: "Booked Event",
+    start: "2022-06-27T12:00:00",
+    end: "2022-06-26T13:00:00",
+    borderColor: "#9231CE",
+    extendedProps: {
+      category: "bookedMeeow",
+    },
+  },
+  {
+    id: 6,
+    title: "On-demand networking",
+    start: "2022-06-27T13:00:00",
+    end: "2022-06-27T13:45:00",
+    borderColor: "#394CED",
+    extendedProps: {
+      category: "onDemandMeeow",
+    },
+  },
+  {
+    id: 7,
+    title: "On-demand networking",
+    start: "2022-06-27T16:00:00",
+    end: "2022-06-27T16:45:00",
+    borderColor: "#394CED",
+    extendedProps: {
+      category: "onDemandMeeow",
+    },
+  },
+  {
+    id: 8,
+    title: "Created Event",
+    start: "2022-06-27T17:00:00",
+    end: "2022-06-27T18:00:00",
+    borderColor: "#00DCDC",
+    extendedProps: {
+      category: "createdMeeow",
+    },
+  },
+];
